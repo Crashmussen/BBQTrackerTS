073f268ca6cc0e32adfc113d0dc7c56620e7a510
Cort Rasmussen <141471802+Crashmussen@users.noreply.github.com>
2023-09-03 21:48:08 -0600

    Update CookItem.tsx

--- src/Components/CookItem.tsx
+++ src/Components/CookItem.tsx
@@ -1,5 +1,16 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { timeConvert } from './utils';
 
-
-export default function CookItem () {
-    
-}
+export default function CookItem({item, remove}) {
+    return (
+        <ListItem secondaryAction={
+            <IconButton edge="end" aria-label="delete" onClick={remove}>
+                <DeleteIcon />
+            </IconButton>
+        }>
+            <ListItemText id={item.id} primary={item.name} secondary={timeConvert(item.time) + " - " + item.temperature +"° F"}/>
+        </ListItem>
+      );
