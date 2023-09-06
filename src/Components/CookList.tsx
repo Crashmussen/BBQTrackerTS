4eb851864e03492d3c15c94d3c6e9eb6138573c7
Cort Rasmussen <141471802+Crashmussen@users.noreply.github.com>
2023-09-05 22:05:21 -0600

    Update CookList.tsx

--- src/Components/CookList.tsx
+++ src/Components/CookList.tsx
@@ -1,5 +1,54 @@
+import { useState, useEffect } from "react"
+import List from "@mui/material/List"
+import CookItem from "./CookItem";
+import CookForm from "./CookForm";
+import './App.css'
 
+const getInitialCooks = () => {
+    const data = JSON.parse(localStorage.getItem("cooks"));
+    if(!data) return [];
+    return data;
+}
 
-export default function CookList () {
+export default function CookList() {
+    const [cooks, setCooks] = useState(getInitialCooks);
+    useEffect(() => {
+        localStorage.setItem(
+            "cooks",
+            JSON.stringify(cooks)
+        )
+    }, [cooks])
+
+    const addCook = (addCook) => {
+        setCooks((prevCooks) => {
+            return [...prevCooks, 
+                    {id: self.crypto.randomUUID(),  
+                    name: addCook.cookName, 
+                    temperature: addCook.cookTemp, 
+                    time: addCook.cookTime}
+            ]
+        })
+    }
+    const removeCook = (id) => {
+        setCooks((prevCooks) => {
+            return prevCooks.filter((c) => c.id !== id)
+        });
+    }
     
+    
+    return (
+        <>
+        <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper"}}>
+            {cooks.map((item) => {
+                return (
+                    <CookItem 
+                        item={item} 
+                        key={item.id} 
+                        remove={() => removeCook(item.id)}/>
+                )
+            })}
+        </List>
+        <CookForm addCook={addCook}/>
+        </>
+    );
 }
